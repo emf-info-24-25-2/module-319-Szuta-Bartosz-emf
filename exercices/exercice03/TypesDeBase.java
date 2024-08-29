e3d26ce105f45c999122fc2e99599c9af8bed827
Szuta-Bartosz-emf <bartosz.szuta@studentfr.ch>
2024-08-29 11:31:11 +0200

    exercise 1

---
+++ exercices/exercice03/TypesDeBase.java
@@ -0,0 +1,13 @@
+package exercices.exercice03;
+
+public class TypesDeBase {public static void main(String[] args) {
+    String nom ="bartosz";
+byte age = 17;
+System.out.println("Je m’appelle " + nom + " et j’ai "+age+" années.");
+byte age1 = 17+10;
+System.out.println("Dans 10 ans, j'aurai " + age1 + " ans.");
+}
+
+    
+}
+    
