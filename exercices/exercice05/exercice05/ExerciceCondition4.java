73fb1e4e9e6fd009e47558c3ad2dfb2766455aab
BuildTools <unconfigured@null.spigotmc.org>
2024-09-07 12:05:21 +0200

    crf

--- exercices/exercice05/exercice05/ExerciceCondition4.java
+++ exercices/exercice05/exercice05/ExerciceCondition4.java
@@ -1,17 +1,22 @@
 package exercices.exercice05.exercice05;
 
+import java.util.Scanner;
+
 public class ExerciceCondition4 {public static void main(String[] args) {
 
-    int température = 3;
-    if (température<-10) {System.out.println("Il fait très froid");
+   Scanner scanner = new Scanner(System.in);
+   System.out.print("Entrez la température : ");
+        int temperature = scanner.nextInt();
+
+    if (temperature<-10) {System.out.println("Il fait très froid");
         
-    } else if (température>=-10 && température<0) {System.out.println("Il fait froid");
+    } else if (temperature>=-10 && temperature<0) {System.out.println("Il fait froid");
         
-    } else if (température>=0 && température<25) {System.out.println("Il fait normal");
+    } else if (temperature>=0 && temperature<25) {System.out.println("Il fait normal");
         
-    }else if (température>=25 && température<=35) {System.out.println("Il fait chaud");
+    }else if (temperature>=25 && temperature<=35) {System.out.println("Il fait chaud");
         
-    }else if (température>35) {System.out.println("Il fait très chaud");
+    }else if (temperature>35) {System.out.println("Il fait très chaud");
         
     }
         
