827fcb7496b5683ae4953a96a5fe5d289a214666
BuildTools <unconfigured@null.spigotmc.org>
2024-09-07 10:53:48 +0200

    Devoir01

---
+++ devoirs/devoir01/devoir01.java
@@ -0,0 +1,19 @@
+package devoirs.devoir01;
+
+public class devoir01 {public static void main(String[] args) { 
+String variable3 = "Je m'appelle Szuta Bartosz";
+System.out.println(variable3);
+int MonAge = 17; 
+System.out.println("Mon âge est de "+ MonAge);
+String variable1 = "estMajeur"; 
+String variable2 = "Je ne suis pas encore majeur";
+if (MonAge>=18) {System.out.println(variable1);
+    
+}else {System.out.println(variable2);}
+
+
+
+    
+}
+    
+}
