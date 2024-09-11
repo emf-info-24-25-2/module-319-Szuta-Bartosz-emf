0eb2af28ce8f24d22f82ff4a9cafcc569165149c
Fanny Riedo <141828170+riedo-emf@users.noreply.github.com>
2024-09-11 23:58:15 +0200

    correction du devoir01

--- devoirs/devoir01/devoir01.java
+++ devoirs/devoir01/devoir01.java
@@ -6,6 +6,12 @@ String variable3 = "Je m'appelle Szuta Bartosz";
 System.out.println(variable3);
 int MonAge = 17; 
 System.out.println("Mon âge est de "+ MonAge);
+    
+//Relisez bien la donnée, String n'est 
+//le bon choix pour estMajeur. Il faut une variable 
+//qui permet de stocker si oui ou non la personne est majeure.
+//changez ceci et corrigez la suite de l'exercice
+    
 String variable1 = "estMajeur"; 
 String variable2 = "Je ne suis pas encore majeur";
 if (MonAge>=18) {System.out.println(variable1);
