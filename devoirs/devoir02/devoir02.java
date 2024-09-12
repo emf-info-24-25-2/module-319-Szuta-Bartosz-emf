39c64cfdfecd0ffe4bfe1d9754dac4cb882fa8f8
BuildTools <unconfigured@null.spigotmc.org>
2024-09-12 22:39:09 +0200

    s

---
+++ devoirs/devoir02/devoir02.java
@@ -0,0 +1,12 @@
+package devoirs.devoir02;
+
+public class devoir02 {public static final int CAPACITE_RESERVOIR_A = 3;
+    public static final int CAPACITE_RESERVOIR_B = 5;
+    public static void main(String[] args) {
+        int reservoirA = 0;
+        int reservoirB = 0;
+        
+        int remplissage = .nextInt(CAPACITE_RESERVOIR_A + CAPACITE_RESERVOIR_B + 1);
+    }
+    
+}
