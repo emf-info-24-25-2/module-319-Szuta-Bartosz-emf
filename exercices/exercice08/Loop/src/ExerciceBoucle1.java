3283fdf36f4406ff6c9991d0859b7eafee9c33e3
BuildTools <unconfigured@null.spigotmc.org>
2024-09-12 21:25:58 +0200

    sa

--- exercices/exercice08/Loop/src/ExerciceBoucle1.java
+++ exercices/exercice08/Loop/src/ExerciceBoucle1.java
@@ -1,23 +1,31 @@
 public class ExerciceBoucle1 {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        for (int i =1;i<6;i++)
-        {if (i==3) continue; {System.out.println(i);}
-            
+        for (int i = 1; i < 6; i++) {
+            if (i == 3)
+                continue;
+            {
+                System.out.println(i);
+            }
+
         }
-         
+
         int i = 1;
-        while (i<6) {System.out.println(i);i++;
-            if (i==4) { break; }
-            
+        while (i < 6) {
+            System.out.println(i);
+            i++;
+            if (i == 4) {
+                break;
+            }
+
         }
 
         int ii = 1;
         do {
-       System.out.println(ii);ii++;
-            
-        } while (ii<6);
+            System.out.println(ii);
+            ii++;
 
+        } while (ii < 6);
 
     }
 
--- exercices/exercice08/Loop/src/ExerciceBoucle2.java
+++ exercices/exercice08/Loop/src/ExerciceBoucle2.java
@@ -1,4 +1,28 @@
-public class ExerciceBoucle2 {public static void main(String[] args) {
-    
-}
+public class ExerciceBoucle2 {
+    public static void main(String[] args) {
+        System.out.println("Boucle for:");
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+        System.out.println("Décollage !!");
+
+        int i = 5;
+        System.out.println("\nBoucle while:");
+        while (i >= 1) {
+            System.out.println(i);
+            i--;
+
+        }
+        System.out.println("Décollage !!");
+
+        System.out.println("\nBoucle do-while:");
+        int j = 5;
+        do {
+            System.out.println(j);
+            j--;
+
+        } while (j >= 1);
+        System.out.println("Décollage !!");
+
+    }
 }
