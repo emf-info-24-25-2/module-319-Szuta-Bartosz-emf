e035dd8d607f601e07d64b5b1980759025f4e164
Szuta-Bartosz-emf <bartosz.szuta@studentfr.ch>
2024-09-28 09:08:52 +0200

    e

--- exercices/exercice06/exercice06/ExerciceSwitch1.java
+++ exercices/exercice06/exercice06/ExerciceSwitch1.java
@@ -3,20 +3,20 @@ package exercices.exercice06.exercice06;
 public class ExerciceSwitch1 {
     public static void main(String[] args) {
         int noteDuModule = 5;
-        /*
-         * if (noteDuModule==1) {System.out.println("Travail non rendu");
-         * 
-         * } else if (noteDuModule==2) {System.out.println("Très insuffisant");
-         * 
-         * }else if (noteDuModule==3) {System.out.println("Insuffisant");
-         * 
-         * } else if (noteDuModule==4) {System.out.println("Suffisant");
-         * 
-         * }else if (noteDuModule==5) {System.out.println("Bien");
-         * 
-         * }else if (noteDuModule==6) {System.out.println("Très bien");
-         */
-        switch (noteDuModule) {
+        
+          if (noteDuModule==1) {System.out.println("Travail non rendu");
+         
+         } else if (noteDuModule==2) {System.out.println("Très insuffisant");
+         
+         }else if (noteDuModule==3) {System.out.println("Insuffisant");
+         
+         } else if (noteDuModule==4) {System.out.println("Suffisant");
+         
+         }else if (noteDuModule==5) {System.out.println("Bien");
+          
+ }else if (noteDuModule==6) {System.out.println("Très bien");
+         
+        /*switch (noteDuModule) {
             case 1:
                 System.out.println("Travail non rendu");
 
@@ -38,7 +38,7 @@ public class ExerciceSwitch1 {
                 break;
 
             default:
-                break;
+                break;*/
     }
     }
 }
