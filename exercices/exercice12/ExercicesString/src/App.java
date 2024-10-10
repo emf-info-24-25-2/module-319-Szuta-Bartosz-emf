e15edc56e2cb1c004a77be5e3cba70b6c2ab9b3c
Szuta-Bartosz-emf <bartosz.szuta@studentfr.ch>
2024-10-10 11:26:29 +0200

    d

---
+++ exercices/exercice12/ExercicesString/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    /*public static void main(String[] args){
+        String maChaineDeCaractere ="Voici du contenu stocké dans un String.";
+        System.out.println(maChaineDeCaractere);
+        System.out.println("le nombre de caractères contenu dans la variable :"+maChaineDeCaractere.length());
+        System.out.println("Le mot \"contenu\" commence à la position : " +maChaineDeCaractere
+        .indexOf("contenu"));
+        System.out.println("quelle est la lettre contenue à la position 10 de la variable :"+maChaineDeCaractere.charAt(9));
+
+
+    }
+    }*/
+    public static void main(String[] args){ 
+        String prenom ="Bartosz";
+        for(int i = 0; i<=7;i++ ) {
+            System.out.println("La lettre à la position "+i+" est: "+prenom.charAt(i));
+            
+        }
+
+
+    }
+}
